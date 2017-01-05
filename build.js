09e267374b212513ffd6a4090582c6ed824bcc50
Schaeff <schaeffer.thibaut@gmail.com>
2017-01-05 12:19:56 +0100

    remove logs

--- build.js
+++ build.js
@@ -1,14 +1,8 @@
 var testlib = require('.')
 var fs = require('fs')
 
-console.log(testlib)
-
 var output = {
 	compiled: testlib
 }
 
-console.log(output)
-
 fs.writeFileSync('dist/testlib.js', "module.exports = " + JSON.stringify(output))
-
-console.log("done!")
